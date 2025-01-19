6e6f1476aa5f8e5a8adeff1ae9a0d1ce0eb98866
Debendra Kumar Bedanta <144777379+bedantadebendra@users.noreply.github.com>
2025-01-19 10:58:18 +0530

    Update vehicle.py

--- Backend/api/models/vehicle.py
+++ Backend/api/models/vehicle.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Vehicle(models.Model):
-    id = models.AutoField(primary_key=True)  # Primary key fieldh=100, unique=True) 
+    id = models.AutoField(primary_key=True) 
+    vehicle_id = models.CharField(max_length=100, unique=True) 
     issue_description = models.TextField() 
     created_at = models.DateTimeField(auto_now_add=True) 
 
