ef1e58b675100198944a889c09579234b1a73b6d
Debendra Kumar Bedanta <144777379+bedantadebendra@users.noreply.github.com>
2025-01-19 11:32:36 +0530

    Update serializers.py

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -14,29 +14,22 @@ class VehicleSerializer(serializers.ModelSerializer):
         fields = ['id','vehicle_number', 'issue_description', 'created_at']
 
 class RepairSerializer(serializers.ModelSerializer):
-    vehicle = serializers.SerializerMethodField()
-    components = serializers.SerializerMethodField()
+    components = serializers.PrimaryKeyRelatedField(queryset=Component.objects.all(), many=True)
 
     class Meta:
         model = Repair
         fields = ['vehicle', 'components', 'total_cost', 'created_at']
 
-    def get_vehicle(self, obj):
-        return {
-            "vehicle_number": obj.vehicle.vehicle_id,
-            "issue_description": obj.vehicle.issue_description,
-        }
-
-    def get_components(self, obj):
-        return [component.name for component in obj.components.all()]
-
     def create(self, validated_data):
         components_data = validated_data.pop('components', [])
+
         repair_instance = Repair.objects.create(**validated_data)
         repair_instance.components.set(components_data)
+
         repair_instance.total_cost = sum(component.repair_price for component in repair_instance.components.all())
+
         repair_instance.save()
-        if repair_instance.total_cost > 0:
+        if repair_instance.total_cost > 0: 
             revenue = Revenue(
                 date=repair_instance.created_at.date(), 
                 revenue_amount=repair_instance.total_cost 
@@ -44,6 +37,7 @@ class RepairSerializer(serializers.ModelSerializer):
             revenue.save()
 
         return repair_instance
+
 class RevenueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Revenue
